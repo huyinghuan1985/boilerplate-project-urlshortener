ed5aaf4c420b406976747b0afacf70e7ccf9caaa
huyinghuan <huyinghuan@qq.com>
2022-11-14 23:58:35 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const dns = require('dns');
+const bodyParser = require('body-parser');
 
-// Basic Configuration
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+let shorturls = [];
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// 获取要被转换的url
+app.post('/api/shorturl', (req, res) => {
+  const original_url = req.body.url;
+  if (!/https*:\/\/.+\..+/.test(original_url))
+    return res.json({ error: 'Invalid URL' });
+  let short_url = shorturls.indexOf(original_url);
+
+  if (short_url === -1) {
+    shorturls.push(req.body.url);
+    short_url = shorturls.length - 1;
+  }
+
+  res.json({ original_url, short_url });
 });
 
-app.listen(port, function() {
+// 解析短网址跳转到真实url
+app.get('/api/shorturl/:short_url', (req, res) => {
+  let short_url = req.params.short_url;
+  if (/[^0-9]+/.test(short_url)) return res.json({ error: 'Wrong format' });
+  let original_url = shorturls[short_url];
+
+  if (original_url === undefined) {
+    return res.json({ error: 'No short URL found for the given input' });
+  }
+
+  res.redirect(302, original_url);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
